9dd96b387081e1496580e63279b758719c436513
Andres <aizarra@yahoo.com>
2022-11-21 12:32:16 +0100

    fix signup bugg

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ app.use('/', indexRoutes);
 const auth = require('./routes/auth');
 app.use('/', auth);
 
-const sessions = require('./routes/sessions');
-app.use('/', sessions);
+// const sessions = require('./routes/sessions');
+// app.use('/', sessions);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
--- routes/auth.js
+++ routes/auth.js
@@ -2,39 +2,38 @@ const router = require('express').Router();
 const User = require('../models/User.model');
 const bcrypt = require('bcryptjs');
 
-router.get('/auth/signup', (req, res, next) => {
-  res.render('signup');
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup');
 });
 
-router.post('/auth/signup', (req, res, next) => {
+router.post('/signup', (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (username === '') {
-    res.render('signup', { message: 'Username cannot be empty' });
+    res.render('auth/signup', { message: 'Username cannot be empty' });
     return;
   }
 
   if (password.length < 4) {
-    res.render('signup', {
+    res.render('auth/signup', {
       message: 'Password has to be minimum 4 characters',
     });
     return;
   }
-   if (!username || !email || !password) {
-     res.render('auth/signup', {
-       errorMessage:
-         'All fields are mandatory. Please provide your username, email and password.',
-     });
-     return;
-   }
+  if (!username || !email || !password) {
+    res.render('auth/signup', {
+      errorMessage:
+        'All fields are mandatory. Please provide your username, email and password.',
+    });
+    return;
+  }
 
   User.findOne({ username }).then((userFromDB) => {
     console.log(userFromDB);
 
     if (userFromDB !== null) {
-      res.render('signup', { message: 'Username is already taken' });
+      res.render('auth/signup', { message: 'Username is already taken' });
     } else {
-     
       const salt = bcrypt.genSaltSync();
       const hash = bcrypt.hashSync(password, salt);
       console.log(hash);
@@ -51,40 +50,39 @@ router.post('/auth/signup', (req, res, next) => {
   });
 });
 //login code
-router.get("/auth/login", (req, res, next) => {
-  res.render("login")
-})
+router.get('/auth/login', (req, res, next) => {
+  res.render('login');
+});
 
-router.post("/auth/login", (req, res, next) => {
-  const { username, password } = req.body
+router.post('/auth/login', (req, res, next) => {
+  const { username, password } = req.body;
 
   // Find user in database by username
-  User.findOne({ username })
-    .then(userFromDB => {
-      if (userFromDB === null) {
-        // User not found in database => Show login form
-        res.render("login", { message: "Wrong credentials" })
-        return
-      }
+  User.findOne({ username }).then((userFromDB) => {
+    if (userFromDB === null) {
+      // User not found in database => Show login form
+      res.render('login', { message: 'Wrong credentials' });
+      return;
+    }
 
-      // User found in database
-      // Check if password from input form matches hashed password from database
-      if (bcrypt.compareSync(password, userFromDB.password)) {
-        // Password is correct => Login user
-        // req.session is an object provided by "express-session"
-        req.session.user = userFromDB
-        res.redirect("/profile")
-      } else {
-        res.render("login", { message: "Wrong credentials" })
-        return
-      }
-    })
-})
+    // User found in database
+    // Check if password from input form matches hashed password from database
+    if (bcrypt.compareSync(password, userFromDB.password)) {
+      // Password is correct => Login user
+      // req.session is an object provided by "express-session"
+      req.session.user = userFromDB;
+      res.redirect('/profile');
+    } else {
+      res.render('login', { message: 'Wrong credentials' });
+      return;
+    }
+  });
+});
 
-router.get("/auth/logout", (req, res, next) => {
+router.get('/auth/logout', (req, res, next) => {
   // Logout user
-  req.session.destroy()
-  res.redirect("/")
-})
+  req.session.destroy();
+  res.redirect('/');
+});
 
-module.exports = router
+module.exports = router;
