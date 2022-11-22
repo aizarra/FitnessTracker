6fd817ed781d9337adbcb15aaa243d5bb75a67d8
khablo1508 <khablo.anna@gmail.com>
2022-11-22 14:52:16 +0100

    rename workout page, adapt route for workout page

--- routes/workouts.routes.js
+++ routes/workouts.routes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const Exercise = require('../models/Exercise.model');
 
-router.get('/workouts', (req, res, next) => {
+router.get('/addWorkout', (req, res, next) => {
   Exercise.find()
     .then((allTheExercisesFromDB) => {
       console.log('Retrieved exercises from DB:', allTheExercisesFromDB);
-      res.render('workouts.hbs', { workouts: allTheExercisesFromDB });
+      res.render('exercises/addWorkout.hbs', {
+        workouts: allTheExercisesFromDB,
+      });
     })
     .catch((error) => {
       console.log('Error while getting the exercises from the DB: ', error);
