b8aab9578eec9184ac5220f7cf6010df2331ccd7
Andres <aizarra@yahoo.com>
2022-11-23 13:51:43 +0100

    changes chosenexercise.route

--- routes/chosenexercises.routes.js
+++ routes/chosenexercises.routes.js
@@ -8,18 +8,14 @@ router.post('/user/chosenExercises', (req, res, next) => {
   console.log('request', req.body.chosenExercise);
   const exercises = req.body.chosenExercise;
   const exercisesObjectId = exercises.map((exercise) => {
-    console.log('exercise', exercise);
     const XR = mongoose.Types.ObjectId(exercise);
-    console.log('XR', XR);
     return XR;
   });
-  console.log('XT', exercisesObjectId);
   User.findByIdAndUpdate(
     req.session.user._id,
     { chosenExercise: exercisesObjectId },
     { new: true }
   ).then((users) => {
-    console.log('newupdated', users);
     res.render('tracker', users);
   });
 });
