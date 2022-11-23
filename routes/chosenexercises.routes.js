01b8c9c99e5d221d769d282235e1ae4b0a209bf6
Andres <aizarra@yahoo.com>
2022-11-23 10:25:20 +0100

    latest

--- routes/chosenexercises.routes.js
+++ routes/chosenexercises.routes.js
@@ -5,23 +5,23 @@ const mongoose = require('mongoose');
 
 router.post('/user/chosenExercises', (req, res, next) => {
   console.log(req.body);
-
-  const chosenExercises = req.body.chosenExercise.map((exercise) => {
-    return mongoose.Types.ObjectId(exercise);
+  const newArray = [];
+  req.body.chosenExercise.forEach((exercise) => {
+    newArray.push(mongoose.Types.ObjectId(exercise));
   });
-
-  User.findOneAndUpdate(
-    { _id: req.session.user._id },
-    { chosenExercise: chosenExercises },
-    { new: true }
-  ).then((User) => {
-    console.log(User);
-    //     res.render('tracker.hbs', { workouts: allTheExercisesFromDB });
-    //   })
-    //   .catch((error) => {
-    //     console.log('Error while getting the exercises from the DB: ', error);
-    //     next(error);
-  });
+  console.log('newarray2', newArray);
+  User.findByIdAndUpdate(
+    req.session.user._id,
+    { $push: { chosenExercise: { $each: newArray } } },
+    { safe: true, upsert: true, new: true }
+  )
+    .then(() => {
+      return User.findById(req.session.user._id);
+    })
+    .then((users) => {
+      console.log('newupdated', users);
+      res.render('tracker', users);
+    });
 });
 
 module.exports = router;
